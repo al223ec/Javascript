6ce51c9bc1098432075c208a212936729883011b
al223ec <al223ec@student.lnu.se>
2013-11-13 10:28:33 +0100

    Föreläsning 2

--- script.js
+++ script.js
@@ -14,8 +14,7 @@
     
         return tal1 + tal2 + tal3 / 3;
     }
-  
-
+  /*
 	var secretNumber = Math.floor((Math.random()*100)+1); 
 	console.log(secretNumber);
 	
@@ -44,5 +43,63 @@
 	{
 		console.log('Inte ett giltigt tal');
 	}
+	*/
+	
+	
+	var minVar = 1; 
+	console.log( typeof minVar ); 
     
+	minVar = "skdoso"; 
+	console.log( typeof minVar ); 
+    
+	minVar = true; 
+	console.log( typeof minVar ); 
+    
+	minVar = 1.300; 
+	console.log( typeof minVar ); 
+	
+	
+	minVar = 'c'; 
+	console.log( typeof minVar ); 
+    
+	var input = prompt('Ange ett tal');
+	
+	if(isNaN(input))
+	{
+		console.log(input);
+	}
+	
+	console.log(+input);
+	
+	var a = +prompt("Ange tal: ");
+	console.log(a + 10);
+	var _name;
+	var _age;
+	
+	function myFunc(name, age)
+	{
+		_name = name || "John Doe"; 
+		_age = age || "18"; 
+	}
+	myFunc();
+	console.log(_name);
+	console.log(_age);
+	
+	function loopFunc()
+	{
+		var counter; 
+		var property;
+		
+		for(counter = 0; counter < 10; counter+=1)
+		{
+			//Gör något
+			console.log(counter);
+		}
+		
+		for(property in document){
+			console.log(document[property]);
+			//Foreach
+			//Obs ordlingen kan inte garanteras
+		}
+	}
 }());
