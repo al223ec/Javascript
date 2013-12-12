ffe118bfdd5ecaf52e1218c8aec08af6cba9788e
al223ec <al223ec@student.lnu.se>
2013-12-12 11:39:23 +0100

    Validation map

--- FunctionTheUltimate.js
+++ FunctionTheUltimate.js
@@ -21,7 +21,7 @@ var digit_name2 = (function () {
 //Fade function
 //The toString() method parses its first argument, and attempts to return a string representation in the 
 //specified radix (base). For radixes above 10, the letters of the alphabet indicate numerals greater than 
-//9. For example, for hexadecimal numbers (base 16), a through f are used.
+//9. For example, for hexadecimal numbers (base 16), a through f are used
 function fade(id) {
     var dom = document.getElementById(id), level = 1;
     function step() {
@@ -63,6 +63,9 @@ function minimizeWidth(id) {
     }
     setTimeout(step, 20);
 };
+
+//Funktioner som kallar sig själv finns alltid kvar och hanteras som objekt, dvs variblarna behåller sina tilldelade värden. 
+//Funktioner hanteras som objekt där variablarna behåller sina värden. 
 console.log(digit_name2(3));
 console.log(digit_name2(3));
 console.log(digit_name2(3));
